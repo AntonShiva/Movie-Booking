6bbb25d641694d491dabdc853c98cf62adc89111
AntonShiva <ryashinanthon@yandex.ru>
2023-10-27 16:31:14 +0300

    Initial Commit

---
+++ Movie Booking/Movie_BookingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movie_BookingApp.swift
+//  Movie Booking
+//
+//  Created by Anton Rasen on 27.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct Movie_BookingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
