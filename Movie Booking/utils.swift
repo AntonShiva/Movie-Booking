9781d98b8b4d3e7aac277889a5444836f8ced486
AntonShiva <ryashinanthon@yandex.ru>
2023-10-30 09:21:24 +0300

    создал верхнюю часть билета Ticket и структуру с расширением для скругление только верхних углов карточки

---
+++ Movie Booking/utils.swift
@@ -0,0 +1,35 @@
+//
+//  utils.swift
+//  Movie Booking
+//
+//  Created by Anton Rasen on 30.10.2023.
+//
+
+import UIKit
+import SwiftUI
+import Foundation
+
+
+// Закругление углов с использованием радиуса угла (что бы скруглить только верхние углы карточки)
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+    
+    func glow(color: Color = .red, radius: CGFloat = 20) -> some View {
+        self
+            .shadow(color: color, radius: radius / 3)
+            .shadow(color: color, radius: radius / 3)
+            .shadow(color: color, radius: radius / 3)
+    }
+}
