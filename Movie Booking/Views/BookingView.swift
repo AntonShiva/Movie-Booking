a1e0de9b3343d524081af69fd9f07ae4a16554bd
AntonShiva <ryashinanthon@yandex.ru>
2023-11-01 20:27:00 +0300

    дописал домашнее предстовление добавил DateButton

---
+++ Movie Booking/Views/BookingView.swift
@@ -0,0 +1,18 @@
+//
+//  BookingView.swift
+//  Movie Booking
+//
+//  Created by Anton Rasen on 01.11.2023.
+//
+
+import SwiftUI
+
+struct BookingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BookingView()
+}
