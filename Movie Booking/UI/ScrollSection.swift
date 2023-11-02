542ec4336d519df505a9cab46e653d0ced690d70
AntonShiva <ryashinanthon@yandex.ru>
2023-11-02 08:14:57 +0300

    создал TimeButton

--- Movie Booking/UI/ScrollSection.swift
+++ Movie Booking/UI/ScrollSection.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 struct ScrollSection: View {
-    @State var title = "Now Playing"
+    @State var title: String = "Now Playing"
     
     @State var posters: [String] = ["poster1", "poster2", "poster3", "poster4", "poster5", "poster6"]
-    
+        
     var body: some View {
         VStack(alignment: .leading) {
             Text(title)
@@ -19,12 +19,11 @@ struct ScrollSection: View {
                 .foregroundColor(.white)
                 .padding(.horizontal, 20)
             
-            
             ScrollView(.horizontal, showsIndicators: false) {
                 HStack(spacing: 20.0) {
                     ForEach(posters.indices, id: \.self) { index in
                         NavigationLink {
-                            Text("Boocking View")
+                            BookingView()
                         } label: {
                             Image(posters[index])
                                 .resizable()
@@ -39,7 +38,6 @@ struct ScrollSection: View {
         }
     }
 }
-
 #Preview {
     ScrollSection()
 }
